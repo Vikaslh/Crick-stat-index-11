7292bbacd4a11409082f380527defd241678cb6c
Vignesh S <vignesh31.sk@gmail.com>
2023-10-28 22:43:17 +0530

    Migrated from juypter to python files. Created py files for batsman, bowlers and wicket_keeper, with visualization of clusters. Created a file run_t20 which selects players from best cluster

---
+++ batsman_t20.py
@@ -0,0 +1,102 @@
+# importing necessary libraries.
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+
+# importing dataset....
+batsman_t20 = pd.read_csv('./datasets/batsman data t20i.csv')
+batsman_t20 = batsman_t20.drop(38)  # removing incorrect data
+
+# features considered.
+features = ['Player Name', 'Batting Average',
+            'Strike Rate', 'Runs', 'Not Outs']
+# cleaning the data
+batsman_t20 = batsman_t20.dropna(subset=features)
+batsman_t20 = batsman_t20[features].copy()
+
+# scaling the data.
+scaler = StandardScaler()
+features.remove('Player Name')
+scaled_data = pd.DataFrame(
+    scaler.fit_transform(batsman_t20[features]),
+    columns=features)
+pca = PCA(2)
+reduced_data = pca.fit_transform(scaled_data)
+
+''' To find how many clusers are to be formed '''
+
+
+def elbow_plot(min_k, max_k, k_max_iter):
+    sum_squared_distances = []
+    k_range = range(min_k, max_k+1)
+    for k in k_range:
+        km = KMeans(n_clusters=k,
+                    max_iter=k_max_iter,
+                    init='k-means++',
+                    n_init=50,
+                    random_state=0)
+        km.fit(reduced_data)
+        sum_squared_distances.append(km.inertia_)
+
+    # Plot the score for each value of k
+    plt.plot(k_range, sum_squared_distances, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Sum of squared distances')
+    plt.title('Elbow Method For Optimal k')
+    plt.show()
+
+
+# elbow_plot(2, 10, 50)
+
+# Clustering the players using k-means algorithm.
+km = KMeans(n_clusters=3,
+            n_init=50,
+            init='k-means++',
+            random_state=0)
+label = km.fit_predict(reduced_data)
+scaled_data['Cluster'] = km.labels_
+
+''' Plot the clusters on a graph...'''
+
+
+def plot_clusters():
+    centroids = km.cluster_centers_
+    u_labels = np.unique(label)
+    for i in u_labels:
+        plt.scatter(reduced_data[label == i, 0],
+                    reduced_data[label == i, 1], label=i)
+    plt.scatter(centroids[:, 0], centroids[:, 1], s=80, color='k')
+    plt.legend()
+    plt.show()
+
+
+# plot_clusters()
+
+scaled_data.insert(0, 'Player Name', batsman_t20['Player Name'])
+
+# print(scaled_data['Cluster'].value_counts())
+
+# spliting the dataframe into diffrent clusters.
+# grp0 = scaled_data.loc[scaled_data['Cluster'] == 0]
+# grp1 = scaled_data.loc[scaled_data['Cluster'] == 1]
+# grp2 = scaled_data.loc[scaled_data['Cluster'] == 2]
+
+# print(len(grp0), len(grp1), len(grp2))
+
+
+def find_best_cluster():
+    idx = -1
+    for i in range(0, 3):
+        min_len = 9999999
+        temp = scaled_data.loc[scaled_data['Cluster'] == i]
+        length = len(temp)
+        if length <= min_len:
+            min_len = length
+            idx = i
+    return scaled_data.loc[scaled_data['Cluster'] == idx]
+
+
+best_batsman = find_best_cluster()
---
+++ run_t20.py
@@ -0,0 +1,34 @@
+from batsman_t20 import best_batsman
+from bowler_t20 import best_bowler
+from keeper_t20 import best_keeper
+
+
+def select_player(batsmen_no, bowler_no, keeper_no):
+    players = []
+    i = 1
+    while i in range(1, batsmen_no+1):
+        temp = best_batsman['Player Name'].sample().tolist()
+        #! Dont forget temp[0].
+        if not temp[0] in players:  # check for duplicates...
+            players.append(temp[0])
+            i += 1
+    i = 1
+    while i in range(1, bowler_no+1):
+        temp = best_bowler['Player Name'].sample().tolist()
+        if not temp[0] in players:  # check for duplicates...
+            players.append(temp[0])
+            i += 1
+    i = 1
+    while i in range(1, keeper_no+1):
+        temp = best_keeper['Player Name'].sample().tolist()
+        if not temp[0] in players:  # check for duplicates...
+            players.append(temp[0])
+            i += 1
+    return players
+
+
+chosen = select_player(3, 3, 3)
+print(chosen)
+
+# select_player(3, 3, 3)
+# print(list(range(1, 3)))
